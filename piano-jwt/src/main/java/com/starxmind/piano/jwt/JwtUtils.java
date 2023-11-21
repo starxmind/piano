9655aff17f5a5b4d42be6e1bbeb18c97891efa57
jianxue <raining0807@163.com>
2023-11-21 22:48:53 +0800

    feat: jwt utils

---
+++ piano-jwt/src/main/java/com/starxmind/piano/jwt/JwtUtils.java
@@ -0,0 +1,43 @@
+package com.starxmind.piano.jwt;
+
+import com.starxmind.bass.json.StarxJson;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * JWT工具
+ *
+ * @author pizzalord
+ * @since 1.0
+ */
+public abstract class JwtUtils {
+    public static String encode(Object object, String secret, long expiration) {
+        Map claims = object instanceof Map ? (Map) object : StarxJson.objectToMap(object);
+        Date now = new Date();
+        String jwt = Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + expiration * 1000))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+        return jwt;
+    }
+
+    public static Map<String, Object> decode(String jwt, String secret) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(jwt).getBody();
+        return claims;
+    }
+
+    public static <T> T decode(String jwt, String secret, Class<T> clazz) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(jwt).getBody();
+        return StarxJson.mapToObject(claims, clazz);
+    }
+}
