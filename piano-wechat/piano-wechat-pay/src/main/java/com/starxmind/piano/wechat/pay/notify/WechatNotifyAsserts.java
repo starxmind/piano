6e0b27eb1f43509947824a92bcdc8be378a03d36
kiwi <kiwi>
2024-06-28 11:05:25 +0800

    feat: wechat pay notify asserts

--- piano-wechat/piano-wechat-pay/src/main/java/com/starxmind/piano/wechat/pay/notify/WechatNotifyAsserts.java
+++ piano-wechat/piano-wechat-pay/src/main/java/com/starxmind/piano/wechat/pay/notify/WechatNotifyAsserts.java
@@ -1,9 +1,12 @@
 package com.starxmind.piano.wechat.pay.notify;
 
+import com.starxmind.bass.sugar.Asserts;
+
 import java.util.Map;
 
 public abstract class WechatNotifyAsserts {
-    public static boolean payOk(Map<String, Object> notifyReq) {
-        return "TRANSACTION.SUCCESS".equals(notifyReq.get("event_type"));
+    public static void payOk(Map<String, Object> notifyReq) {
+        Asserts.equals("TRANSACTION.SUCCESS", notifyReq.get("event_type"),
+                "Wechat notify request is not 'SUCCESS'!");
     }
 }
