9b386ca8731ba6bee4e705dc128d048566d5367c
kiwi <kiwi>
2024-07-09 11:27:46 +0800

    Revert "feat: redisLock"
    This reverts commit c1d78c6e7bfd63b63b08f302995a8d46c1e030be.

--- piano-redis/src/main/java/com/starxmind/piano/redis/DistributedLock.java
+++ piano-redis/src/main/java/com/starxmind/piano/redis/DistributedLock.java
@@ -1,7 +1,6 @@
 package com.starxmind.piano.redis;
 
 import com.starxmind.piano.redis.exceptions.LockException;
-import com.starxmind.piano.redis.utils.KeyUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
@@ -17,64 +16,43 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class DistributedLock {
     private final RedissonClient redissonClient;
+    private final String lockKey;
+    private final RLock internalLock;
 
-    public DistributedLock(RedissonClient redissonClient) {
+    public DistributedLock(RedissonClient redissonClient, String lockKey) {
         this.redissonClient = redissonClient;
+        this.lockKey = lockKey;
+        this.internalLock = initInternalLock();
     }
 
-    private RLock getLock(String lockKey) {
-        return redissonClient.getLock(KeyUtils.lockKey(lockKey));
+    private RLock initInternalLock() {
+        return redissonClient.getLock(lockKey);
     }
 
-    public boolean isLocked(String lockKey) {
-        RLock lock = getLock(lockKey);
-        return lock != null && lock.isLocked();
+    public boolean isLock() {
+        return internalLock.isLocked();
     }
 
-    public boolean isHeldByCurrentThread(String lockKey) {
-        RLock lock = getLock(lockKey);
-        return lock.isHeldByCurrentThread();
+    public boolean isHeldByCurrentThread() {
+        return internalLock.isHeldByCurrentThread();
     }
 
-    public void lock(String lockKey) {
-        RLock lock = getLock(lockKey);
-        lock.lock();
+    public void lock(long leaseTime, TimeUnit unit) {
+        internalLock.lock(leaseTime, unit);
     }
 
-    public void lock(String lockKey, long leaseTime, TimeUnit unit) {
-        RLock lock = getLock(lockKey);
-        lock.lock(leaseTime, unit);
-    }
-
-    public boolean tryLock(String lockKey) {
-        RLock lock = getLock(lockKey);
-        return lock.tryLock();
-    }
-
-    public boolean tryLock(String lockKey, long waitTime, TimeUnit unit) {
+    public boolean tryLock(long waitTime, long leaseTime, TimeUnit unit) {
         try {
-            RLock lock = getLock(lockKey);
-            return lock.tryLock(waitTime, unit);
+            return internalLock.tryLock(waitTime, leaseTime, unit);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new LockException(String.format("Acquire lock fail by thread interrupted,path:%s", lockKey), e);
         }
     }
 
-    public boolean tryLock(String lockKey, long waitTime, long leaseTime, TimeUnit unit) {
+    public void unlock() {
         try {
-            RLock lock = getLock(lockKey);
-            return lock.tryLock(waitTime, leaseTime, unit);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new LockException(String.format("Acquire lock fail by thread interrupted,path:%s", lockKey), e);
-        }
-    }
-
-    public void unlock(String lockKey) {
-        try {
-            RLock lock = getLock(lockKey);
-            lock.unlock();
+            internalLock.unlock();
         } catch (IllegalMonitorStateException ex) {
             log.warn("Unlock path:{} error for thread status change in concurrency", lockKey, ex);
         }
