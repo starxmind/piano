6be14509632202653c24a5e3153a979a38eb44a6
pizzalord <raining0807@163.com>
2024-11-03 21:03:09 +0800

    fix: PayPackage NoArgsConstructor

--- piano-wechat/piano-wechat-pay/src/main/java/com/starxmind/piano/wechat/pay/resp/PayPackage.java
+++ piano-wechat/piano-wechat-pay/src/main/java/com/starxmind/piano/wechat/pay/resp/PayPackage.java
@@ -1,11 +1,15 @@
 package com.starxmind.piano.wechat.pay.resp;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
+@Builder(toBuilder = true)
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class PayPackage {
     private String prepayId;
